16ae433d5597e72ddad8475a2aec049f400999c0
Emerson Javid Gonzalez Morales <1388310@SOAM.TCS.com>
2017-09-04 14:42:44 -0500

    Exercise 1: Create a service. In this exercise we will create and declare a new service.

---
+++ MyDownloader/MyDownloadService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Util;
+
+namespace MyDownloader
+{
+    [Service(Label = "MyDownloadService", Icon = "@drawable/Icon")]
+    public class MyDownloadService : Service
+    {
+        const string tag = "MyDownloadService";
+
+        public override IBinder OnBind(Intent intent)
+        {
+            Log.Debug(tag, "OnBind called");
+            throw new NotImplementedException();
+        }
+
+        public override StartCommandResult OnStartCommand(Intent intent,//The Intent used to start the service
+            StartCommandFlags flags, //Indicates whether the service was restarted
+            int startId)//A unique integer for this call to start the service
+        {
+            Log.Debug(tag, "OnStartCommand called");
+            return StartCommandResult.RedeliverIntent;//Restarts with the original intent
+        }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+            Log.Debug(tag, "Service created");
+        }
+
+        public override void OnDestroy()
+        {
+            Log.Debug(tag, "Service destroyed");
+        }
+    }
+}
